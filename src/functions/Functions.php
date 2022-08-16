7bebd8b49ea6b38b72632806688bf7fc211727a7
Andrey Bakayev <andreybakayev@gmail.com>
2022-08-16 21:20:15 +0300

    Hometask3 - Functions

--- src/functions/Functions.php
+++ src/functions/Functions.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * php version 8.1.9
+
  * Create the next tests in tests folder for next methods:
  * sayHello(), sayHelloArgument(), sayHelloArgumentWrapper(), countArguments(), countArgumentsWrapper().
  * Test should be placed in folder tests.
@@ -19,6 +21,11 @@
 
 namespace functions;
 
+use PHPUnit\Framework\Constraint\IsNull;
+
+/**
+ * Functions Class of functions which says Hello :)  and functions that demonstrates the func_get_args() function
+ */
 class Functions
 {
     /**
@@ -35,9 +42,13 @@ class Functions
     /**
      * Create a PhpUnit test (SayHelloArgumentTest) which will check that function below returns a correct result
      * Check how it works with: number, string, bool value
+     * 
+     * Feature: with bool value true it returns "Hello 1", false or null returns "Hello "
+     * Only scalar types, Do not accepts arrays!
      *
-     * @param $arg
-     * @return string
+     * @param $arg Parameter to return afret Hello word
+     * 
+     * @return string returns string like "Hello Mike"
      */
     public function sayHelloArgument($arg): string
     {
@@ -51,14 +62,19 @@ class Functions
      * Create a PhpUnit test (SayHelloArgumentWrapperTest) which will check this behavior
      * !!! you need to test only exceptional case, since the behavior of sayHelloArgument was already tested in the task above
      *
-     * @param $arg
-     * @return string
+     * @param $arg argument that verifyed to be a number, string or bool and call the sayHelloArgument with it
+     * 
+     * @return string safely returns "Hello $arg" string
+     * 
      * @throws \InvalidArgumentException
      */
     public function sayHelloArgumentWrapper($arg): string
     {
         // put your code here
-
+        if (!is_scalar($arg) && !is_null($arg)) {
+            throw new \InvalidArgumentException('sayHelloArgument() function accepts only scalar types: number, string or bool. ');
+            return "Hello";//
+        }
         return $this->sayHelloArgument($arg);
     }
 
@@ -84,6 +100,7 @@ class Functions
      * !!! you need to test only exceptional case, since the behavior of countArguments was already tested in the task above
      *
      * You will need to use "Argument unpacking via ..." to pass the parameters to wrapped function:
+     *
      * @see https://www.php.net/manual/en/migration56.new-features.php#migration56.new-features.splat
      *
      * @return array
@@ -91,6 +108,12 @@ class Functions
      */
     public function countArgumentsWrapper(): array
     {
-        // put your code here
+        foreach (func_get_args() as $val) {
+            if (!is_string($val)) {
+                throw new \InvalidArgumentException('countArgumentsWrapper() function accepts strings only');
+            }
+        }
+        return $this->countArguments(...func_get_args());
+
     }
 }
