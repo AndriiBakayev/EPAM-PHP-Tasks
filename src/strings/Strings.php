97ac6cb0f2c9c094c62bf12f8c73ac774271d8e2
Andrey Bakayev <andreybakayev@gmail.com>
2022-08-16 23:15:59 +0300

    minor doc updates

--- src/strings/Strings.php
+++ src/strings/Strings.php
@@ -1,26 +1,39 @@
 <?php
 
 /**
- * Create the Strings Class and implement the StringsInterface and following methods:
- * snakeCaseToCamelCase(), mirrorMultibyteString(), getBrandName()
- * See details below.
- *
- * Note: Use next namespace for Strings Class - "namespace strings;" (Like in this Interface)
- * About composer autoloading and namespaces you can read here -
- * https://www.phptutorial.net/php-oop/php-composer-autoload/
+ * Php version 8.1.9
+ * 
+ * @category No_Category
+ * @package  No_Package
+ * @author   Andrey Bakayev <andreybakayev@gmail.com>
+ * @license  https://github.com/AndriiBakayev free
+ * @link     https://github.com/AndriiBakayev
  */
 
 namespace strings;
 
+/**
+ * Strings Class and implements the StringsInterface and following methods:
+ * snakeCaseToCamelCase(), mirrorMultibyteString(), getBrandName()
+ * See details below.
+ * 
+ * @category No_Category
+ * @package  No_Package
+ * @author   Andrey Bakayev <andreybakayev@gmail.com>
+ * @license  https://github.com/AndriiBakayev free
+ * @link     https://github.com/AndriiBakayev
+ */
 class Strings implements StringsInterface
 {
     /**
-     * The $input variable contains text in snake case (i.e. hello_world or this_is_home_task)
-     * Transform it into camel cased string and return (i.e. helloWorld or thisIsHomeTask)
+     * The $input variable contains text in snake case 
+     * (i.e. hello_world or this_is_home_task)
+     * Transform it into camel cased string and return 
+     * (i.e. helloWorld or thisIsHomeTask)
+     * see http://xahlee.info/comp/camelCase_vs_snake_case.html
      *
-     * @see http://xahlee.info/comp/camelCase_vs_snake_case.html
-     *
-     * @param  string $input string in "snake-case"
+     * @param string $input string in "snake-case"
+     * 
      * @return string $output string in "camelCase"
      */
     public function snakeCaseToCamelCase(string $input): string
@@ -35,7 +48,8 @@ class Strings implements StringsInterface
      * Mirror each word individually and return transformed text (i.e. 'АВЫФ ждло')
      * !!! do not change words order
      *
-     * @param  string $input text of words with " " delimiter
+     * @param string $input text of words with " " delimiter
+     * 
      * @return string output string with mirrored words
      */
     public function mirrorMultibyteString(string $input): string
@@ -50,17 +64,21 @@ class Strings implements StringsInterface
 
     /**
      * My friend wants a new band name for her band.
-     * She likes bands that use the formula: 'The' + a noun with first letter capitalized.
+     * She likes bands that use the formula: 'The' + a noun 
+     * with first letter capitalized.
      * However, when a noun STARTS and ENDS with the same letter,
-     * she likes to repeat the noun twice and connect them together with the first and last letter,
+     * she likes to repeat the noun twice and connect them together
+     *  with the first and last letter,
      * combined into one word like so (WITHOUT a 'The' in front):
      * dolphin -> The Dolphin
      * alaska -> Alaskalaska
      * europe -> Europeurope
      * Implement this logic.
      *
-     * @param  string $noun of brand
-     * @return string output string with repeated noun if anagrama and "The" prefix if it noncapitalized
+     * @param string $noun of brand
+     * 
+     * @return string output string with repeated noun if anagrama 
+     * and "The" prefix if it noncapitalized
      */
     public function getBrandName(string $noun): string
     {
