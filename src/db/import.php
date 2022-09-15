601cd15670f2e11267be2f9af95de2ecf4bae9f1
Andrii Bakayev <andreybakayev@gmail.com>
2022-09-15 13:25:21 +0300

    hometask7-db done

--- src/db/import.php
+++ src/db/import.php
@@ -1,35 +1,80 @@
 <?php
+
 /**
  * TODO
  *  Open web/airports.php file
  *  Go through all airports in a loop and INSERT airports/cities/states to equivalent DB tables
- *  (make sure, that you do not INSERT the same values to the cities and states i.e. name should be unique i.e. before INSERTing check if record exists)
+ *  (make sure, that you do not INSERT the same values to the cities and states i.e. name should be unique
+ *  i.e. before INSERTing check if record exists)
  */
 
 /** @var \PDO $pdo */
 require_once './pdo_ini.php';
-
-foreach (require_once('../web/airports.php') as $item) {
-    // Cities
-    // To check if city with this name exists in the DB we need to SELECT it first
-    $sth = $pdo->prepare('SELECT id FROM cities WHERE name = :name');
-    $sth->setFetchMode(\PDO::FETCH_ASSOC);
-    $sth->execute(['name' => $item['city']]);
-    $city = $sth->fetch();
-
-    // If result is empty - we need to INSERT city
-    if (!$city) {
-        $sth = $pdo->prepare('INSERT INTO cities (name) VALUES (:name)');
+require_once './migrations.php';
+try {
+    $pdo->beginTransaction();
+    $countInserted = $countAlreadyInDB = 0;
+    foreach (require_once('../web/airports.php') as $item) {
+        // Cities
+        // To check if city with this name exists in the DB we need to SELECT it first
+        $sth = $pdo->prepare('SELECT id FROM cities WHERE name = :name');
+        $sth->setFetchMode(\PDO::FETCH_ASSOC);
         $sth->execute(['name' => $item['city']]);
-
-        // We will use this variable to INSERT airport
-        $cityId = $pdo->lastInsertId();
-    } else {
-        // We will use this variable to INSERT airport
-        $cityId = $city['id'];
+        $city = $sth->fetch();
+        // If result is empty - we need to INSERT city
+        if (!$city) {
+            $sth = $pdo->prepare('INSERT INTO cities (name) VALUES (:name)');
+            $sth->execute(['name' => $item['city']]);
+            // We will use this variable to INSERT airport
+            $cityId = $pdo->lastInsertId();
+        } else {
+            // We will use this variable to INSERT airport
+            $cityId = $city['id'];
+        }
+        // States
+        // To check if state with this name exists in the DB we need to SELECT it first
+        $sth = $pdo->prepare('SELECT id FROM states WHERE name = :name');
+        $sth->setFetchMode(\PDO::FETCH_ASSOC);
+        $sth->execute(['name' => $item['state']]);
+        $state = $sth->fetch();
+        // If result is empty - we need to INSERT state
+        if (!$state) {
+            $sth = $pdo->prepare('INSERT INTO states (name) VALUES (:name)');
+            $sth->execute(['name' => $item['state']]);
+            // We will use this variable to INSERT airport
+            $stateId = $pdo->lastInsertId();
+        } else {
+            // We will use this variable to INSERT airport
+            $stateId = $state['id'];
+        }
+        //  Airports
+        // To check if airport with code in the DB we need to SELECT it first
+        $sth = $pdo->prepare('SELECT code FROM airports WHERE code = :code');
+        $sth->setFetchMode(\PDO::FETCH_ASSOC);
+        $sth->execute(['code' => $item['code']]);
+        $airport = $sth->fetch();
+        // If result is empty - we need to INSERT airport
+        if (!$airport) {
+            $sth = $pdo->prepare('INSERT INTO airports (name, code, city_id, state_id, address, timezone) 
+                VALUES (:airoport_name,:code,:city_id,:state_id,:address,:timezone)');
+            $sth->execute(
+                ['airoport_name' => $item['name'],
+                'code' => $item['code'],
+                'city_id' => $cityId,
+                'state_id' => $stateId,
+                'address' => $item['address'],
+                'timezone' => $item['timezone']]
+            );
+            $countInserted++;
+        } else {
+            // We will use this variable to INSERT airport
+            $cityId = $city['id'];
+            $countAlreadyInDB++;
+        }
     }
-
-    // TODO States
-
-    // TODO Airports
+    $pdo->commit();
+    echo "Airports.php parsed. Inseted $countInserted, alreadyInDB $countAlreadyInDB";
+} catch (PDOException $e) {
+    $pdo->rollBack();
+    die("DB ERROR: " . $e->getMessage());
 }
--- src/db/migrations.php
+++ src/db/migrations.php
@@ -1,39 +1,67 @@
 <?php
+
 /**
  * TODO
  *  Write DPO statements to create following tables:
  *
  *  # airports
- *   - id (unsigned int, autoincrement)
- *   - name (varchar)
- *   - code (varchar)
+ *   - id (unsigned INT, autoincrement)
+ *   - name (VARCHAR)
+ *   - code (VARCHAR)
  *   - city_id (relation to the cities table)
  *   - state_id (relation to the states table)
- *   - address (varchar)
- *   - timezone (varchar)
+ *   - address (VARCHAR)
+ *   - timezone (VARCHAR)
  *
  *  # cities
- *   - id (unsigned int, autoincrement)
- *   - name (varchar)
+ *   - id (unsigned INT, autoincrement)
+ *   - name (VARCHAR)
  *
  *  # states
- *   - id (unsigned int, autoincrement)
- *   - name (varchar)
+ *   - id (unsigned INT, autoincrement)
+ *   - name (VARCHAR)
  */
 
 /** @var \PDO $pdo */
 require_once './pdo_ini.php';
-
+// drop tables
+// $sql = <<<'SQL'
+// drop table if exists airports;
+// drop table if exists cities;
+// drop table if exists states;
+// SQL;
+// $pdo->exec($sql);
 // cities
 $sql = <<<'SQL'
-CREATE TABLE `cities` (
-	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(50) NOT NULL COLLATE 'utf8_general_ci',
+CREATE TABLE IF NOT EXISTS `cities` (
+	`id` INT UNSIGNED AUTO_INCREMENT,
+	`name` VARCHAR(40) NOT NULL COLLATE 'utf8_general_ci',
 	PRIMARY KEY (`id`)
 );
 SQL;
 $pdo->exec($sql);
-
-// TODO states
-
-// TODO airports
+// states
+$sql = <<<'SQL'
+CREATE TABLE IF NOT EXISTS `states` (
+	`id` INT UNSIGNED AUTO_INCREMENT,
+	`name` VARCHAR(40) NOT NULL COLLATE 'utf8_general_ci',
+	PRIMARY KEY (`id`)
+);
+SQL;
+$pdo->exec($sql);
+// airports
+$sql = <<<'SQL'
+CREATE TABLE IF NOT EXISTS airports(
+	`id` INT UNSIGNED AUTO_INCREMENT,
+	`name` VARCHAR(100) NOT NULL,
+	`code` VARCHAR(3) not null,
+	`city_id` INT UNSIGNED, KEY `city_id` (`city_id`), 
+	CONSTRAINT `c_airport_city` FOREIGN KEY (`city_id`) REFERENCES `cities` (`id`),
+	`state_id` INT UNSIGNED, KEY `state_id` (`state_id`), 
+	CONSTRAINT `c_airport_state` FOREIGN KEY (`state_id`) REFERENCES `states` (`id`),
+	`address` VARCHAR(150) ,
+	`timezone` VARCHAR(40),
+	PRIMARY KEY (`id`)
+);
+SQL;
+$pdo->exec($sql);
